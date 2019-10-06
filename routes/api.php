1722508927c45da6905235cce95bb30922717d3d
Chinedu <eduvin2011@gmail.com>
2019-10-06 19:52:11 +0100

    Testing

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//route for spa not needed yet
+Route::get('{any}', 'HomeController@index')->where('any','.*');
